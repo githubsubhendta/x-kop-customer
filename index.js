702d6b1ac8b8e215139de4d5e264b0334a3d2c35
dev-eshopgenius <dev.eshopgenius@gmail.com>
2024-09-28 16:01:32 +0530

    change code

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ import {name as appName} from './app.json';
 import { setupNotificationChannel, initializeFirebaseMessaging, setupNotificationListeners } from './src/notification/callNotificationService.js';
 
 AppRegistry.registerComponent(appName, () => () => {
-  setupNotificationChannel(); // Ensure the channel is set up when the app starts
-  initializeFirebaseMessaging(); // Initialize Firebase messaging
-  setupNotificationListeners(); // Set up Notifee background event listeners
+  setupNotificationChannel(); 
+  initializeFirebaseMessaging(); 
+  setupNotificationListeners(); 
     return <App />;
   });
