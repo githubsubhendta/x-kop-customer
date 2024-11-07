cc83b9f1b4c2cd56ef4f75e40cf96d220bd927d5
nickseth71 <nickseth71@gmail.com>
2024-11-07 18:51:11 +0530

    cnage codE

--- src/Api/ApiManager.js
+++ src/Api/ApiManager.js
@@ -1,5 +1,5 @@
 // const bUI = 'https://xkop.in/';
 
-const bUI='https://a27f-49-36-211-102.ngrok-free.app/';
+const bUI='https://bb9b-2405-201-6007-5823-3014-8d0a-d429-ad5a.ngrok-free.app/';
 export const BASE_URI = bUI + 'api/v1';
 export const BASE_URI_websocket = bUI;
