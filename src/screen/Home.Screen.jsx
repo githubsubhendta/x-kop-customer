ea37a2ebe7d517b8ac9ae73cf8da61cb85000c9b
nickseth71 <nickseth71@gmail.com>
2024-11-26 17:27:13 +0530

    chnage code

--- src/screen/Home.Screen.jsx
+++ src/screen/Home.Screen.jsx
@@ -124,7 +124,6 @@
 
 // export default HomeScreen;
 
-
 import React, {useEffect} from 'react';
 import {
   StatusBar,
@@ -156,12 +155,15 @@ const HomeScreen = ({handleTabPress}) => {
       <StatusBar barStyle="dark-content" backgroundColor="#fff" />
       <ScrollView className="flex-1">
         <View className="flex justify-center items-center h-[800px] px-5">
-          <Text className="text-primary text-xl font-medium mt-10 text-center">
-            Welcome to ExKop
+          <Text className="text-primary text-xl font-medium mt-10 text-center" style={{fontFamily:"Ubuntu-R"}}>
+            {/* Welcome to ExKop */}
+            Empowering Your Success with Expert Legal Insights
           </Text>
-          <Text className="text-secondary w-full text-center px-5 text-sm mt-2">
-            Consult expertises in the field and discover the correct ways to
-            your problems. Empower today!
+          <Text className="text-secondary w-full text-center px-5 text-sm mt-2" style={{fontFamily:"Titillium-Regular"}}>
+            {/* Consult expertises in the field and discover the correct ways to
+            your problems. Empower today! */}
+            Tap into our network of seasoned law enforcement professionals for
+            guidance and security.
           </Text>
 
           <View className="mt-16">
@@ -170,18 +172,17 @@ const HomeScreen = ({handleTabPress}) => {
               onPress={() => {
                 navigate('SelectConsultantsScreen');
               }}>
-              <Text className="text-lg font-medium text-white text-center">
+              <Text className="text-lg font-medium text-white text-center" style={{fontFamily:"Titillium-Regular"}}>
                 Start Consultation
               </Text>
             </TouchableOpacity>
             <TouchableOpacity
               className="mt-4 rounded-lg"
               onPress={() => {
-                navigate('Schedule')
+                navigate('Schedule');
                 handleTabPress('Schedule');
-              }}
-            >
-              <Text className="text-lg font-medium text-primary text-center">
+              }}>
+              <Text className="text-lg font-medium text-primary text-center" style={{fontFamily:"Titillium-Regular"}}>
                 Schedule For Later
               </Text>
             </TouchableOpacity>
@@ -197,37 +198,58 @@ const HomeScreen = ({handleTabPress}) => {
 
         <View className="flex-1 justify-center mt-10 mx-5">
           <View className="w-full mx-auto">
+            {/* ./../images/home2.png */}
             <Image
-              source={require('./../images/home2.png')}
+              source={require('./../images/xkop-home1.png')}
               className="w-full h-40 object-cover rounded-lg"
               resizeMode="cover"
             />
           </View>
-          <Text className="text-primary text-lg font-medium pt-5">
-            Empowering Individual
+          <Text className="text-primary text-lg font-medium pt-5" style={{fontFamily:"Ubuntu-R",fontWeight:"700"}}>
+            {/* Empowering Individual */}
+            Unrivaled Wisdom and Expertise
           </Text>
-          <Text className="text-secondary text-base font-light mt-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-            risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
-            nec, ultricies sed, dolor.
+          <Text className="text-secondary text-base font-light mt-2" style={{fontFamily:"Titillium-Regular"}}>
+            Benefit from the wisdom of retired officers who bring a wealth of
+            knowledge from their illustrious careers in law enforcement to every
+            case.
+          </Text>
+        </View>
+
+        <View className="flex-1 justify-center mt-10 mx-5 mb-20">
+          <View className="w-full mx-auto">
+            {/* ./../images/home1.png */}
+            <Image
+              source={require('./../images/xkop-home2.png')}
+              className="w-full h-40 object-cover rounded-lg"
+              resizeMode="cover"
+            />
+          </View>
+          <Text className="text-primary text-lg font-medium pt-5" style={{fontFamily:"Ubuntu-R",fontWeight:"700"}}>
+            Democratizing Legal Guidance
+          </Text>
+          <Text className="text-secondary text-base font-light mt-2" style={{fontFamily:"Titillium-Regular"}}>
+            Empowering the common man by making expert legal advice accessible
+            and affordable, ensuring that no issue is too small for professional
+            attention.
           </Text>
         </View>
 
         <View className="flex-1 justify-center mt-10 mx-5 mb-20">
           <View className="w-full mx-auto">
             <Image
-              source={require('./../images/home1.png')}
+              source={require('./../images/xkop-home3.png')}
               className="w-full h-40 object-cover rounded-lg"
               resizeMode="cover"
             />
           </View>
-          <Text className="text-primary text-lg font-medium pt-5">
-            Lorem Ipsum
+          <Text className="text-primary text-lg font-medium pt-5" style={{fontFamily:"Ubuntu-R",fontWeight:"700"}}>
+            Strategic Corporate Partnerships
           </Text>
-          <Text className="text-secondary text-base font-light mt-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-            risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
-            nec, ultricies sed, dolor.
+          <Text className="text-secondary text-base font-light mt-2" style={{ fontFamily: "Titillium-Regular"}}>
+            We offer comprehensive support for businesses, handling everything
+            from routine legal inquiries to complex fraud investigations,
+            ensuring your corporate interests are protected and advanced
           </Text>
         </View>
       </ScrollView>
@@ -243,4 +265,4 @@ const styles = StyleSheet.create({
 });
 
 export default HomeScreen;
-
+ 
