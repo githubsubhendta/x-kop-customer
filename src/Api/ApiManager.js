f4a10f902706fec0e95c03e43f3ed9109b650e53
nickseth71 <nickseth71@gmail.com>
2024-12-09 13:12:57 +0530

    change code

--- src/Api/ApiManager.js
+++ src/Api/ApiManager.js
@@ -2,4 +2,4 @@ const bUI = 'https://xkop.in/';
 
 // const bUI='https://66ce-49-36-211-102.ngrok-free.app/';
 export const BASE_URI = bUI + 'api/v1';
-export const BASE_URI_websocket = bUI;
+export const BASE_URI_websocket = bUI+"websocket";
